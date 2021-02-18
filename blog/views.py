d39319e15c093f0e42bfb1cb1f9d5519f004b473
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-02-19 00:36:06 +0100

    update views

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect, reverse
 from .models import Post
 from django.views.generic import ListView, DetailView
+from django.http import JsonResponse, HttpResponse
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
@@ -17,3 +20,26 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post-details.html'
     context_object_name = 'post'
+
+
+    # the like button view
+@login_required
+def post_like(request):
+    # if request.method == 'POST' and request.is_ajax():
+    if request.POST.get('action') == 'post':
+        print(request.POST.get('action'))
+        flag = None
+        postid = int(request.POST.get('post_id'))
+        post_obj = get_object_or_404(Post, id=postid)
+
+        if post_obj.likes.filter(id=request.user.id).exists():
+            post_obj.likes.remove(request.user)
+            post_obj.save()
+            flag = False
+        else:
+            post_obj.likes.add(request.user)
+            post_obj.save()
+            flag = True
+        
+        return JsonResponse({'total_likes': post_obj.total_likes, 'flag': flag,})
+    return HttpResponse("Error access denied")
