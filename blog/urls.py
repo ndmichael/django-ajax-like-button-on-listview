884b0007e40878e6cd0ce15fabc8fa8bb2a94b0c
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-02-19 00:35:03 +0100

    updated the blog url

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, post_like
+from blog import views
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', PostListView.as_view()),
     path('details/<int:pk>/', PostDetailView.as_view()),
+    path('like/', post_like, name='post_like'),
 ]
+
